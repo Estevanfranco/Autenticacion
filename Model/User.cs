d7c0db83fffaf257258577d384a4ba0badb6b944
estevan franco abadia <drago2062@gmail.com>
2024-11-06 19:24:40 -0500

    Creacion de login

---
+++ Model/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Autenticacion.Model
+{
+    public class User
+    {
+
+        [Required] // verificar que se importó using System.ComponentModel.DataAnnotations;
+        [DataType(DataType.EmailAddress)]
+        
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        
+        public string Password { get; set; }
+
+    }
+}
